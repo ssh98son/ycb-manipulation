080f533af084b737b22926f210baf975f7d9bd25
ssh98son <ssh199898@gmail.com>
2022-12-25 14:03:06 +0900

    [Add] getObjectNames()

--- ycb_manipulation/__init__.py
+++ ycb_manipulation/__init__.py
@@ -4,3 +4,15 @@ import os
 def getDataPath():
   resdir = os.path.join(os.path.dirname(__file__))
   return resdir
+
+
+def getObjectNames():
+  dir = getDataPath()
+  names = []
+  for name in os.listdir(dir):
+    if "0" not in name:
+      continue
+
+    names.append(name)
+
+  return names
